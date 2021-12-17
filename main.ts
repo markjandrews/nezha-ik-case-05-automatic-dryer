c866a80982a31f135b5d8cb607bd85720f13f5ed
Mark <mark.andrews@internode.on.net>
2021-12-18 09:42:59 +1100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,11 @@
-basic.showIcon(IconNames.House)
+let i = 0
+basic.showIcon(IconNames.Fabulous)
 basic.forever(function () {
-	
+    i = PlanetX_Basic.ultrasoundSensor(PlanetX_Basic.DigitalRJPin.J1, PlanetX_Basic.Distance_Unit_List.Distance_Unit_cm)
+    if (i > 4 && i <= 20) {
+        neZha.setMotorSpeed(neZha.MotorList.M1, 100)
+        basic.pause(5000)
+    } else {
+        neZha.stopMotor(neZha.MotorList.M1)
+    }
 })
